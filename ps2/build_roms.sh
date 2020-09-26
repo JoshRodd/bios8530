27db47dce88d6a76c4caa87fbc11d7e0e7d2d89e
Josh Rodd <josh@rodd.us>
2020-09-26 12:27:43 -0400

    Move files

--- ps2/build_roms.sh
+++ ps2/build_roms.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export PATH=$PATH:"$(pwd)"/../src
-SRC_DIR="$(pwd)"/
+BIN_DIR="$(pwd)"/bin
+SPLICE=$(pwd)/bin/splice
+BASE_DIR="$(pwd)"/
 BUILD_DIR="$(pwd)"/build/
 DIST_DIR="$(pwd)"/dist/
 ARCHIVES_DIR="$(pwd)"/archives/
@@ -22,10 +23,10 @@ rm -f IBM_PS2-30/61X8937_A58470_IBM87.bin || exit
 rm -f IBM_PS2-30/61X8938_A58470_IBM87.bin || exit
 rm -f 61X8938_A58470_IBM87nwe26sept2020 || exit
 rmdir IBM_PS2-30 2>/dev/null
-unzip ../archives/IBM_PS2-30.zip || exit
+unzip "$ARCHIVES_DIR"IBM_PS2-30.zip || exit
 mv IBM_PS2-30/61X8938_A58470_IBM87.bin 61X8938.BAD || exit # This is a known bad file.
 mv IBM_PS2-30/61X8937_A58470_IBM87.bin 61X8937.BIN || exit
-unzip ../archives/61X8938_A58470_IBM87nwe26sept2020.zip || exit
+unzip "$ARCHIVES_DIR"61X8938_A58470_IBM87nwe26sept2020.zip || exit
 mv 61X8938_A58470_IBM87nwe26sept2020 61X8938.BIN || exit # This is a replacement good file.
 rmdir IBM_PS2-30 || exit
 
@@ -34,6 +35,6 @@ while read model even odd; do
     odd="$(basename "$odd" "$fileext")"
     echo
     echo -n $model ROM from $even and $odd:' '
-    splice --verbose "$BUILD_DIR""$even""$fileext" "$BUILD_DIR""$odd""$fileext" "$BUILD_DIR""$even"_"$odd"_"$model""$fileext"
+    "$SPLICE" --verbose "$BUILD_DIR""$even""$fileext" "$BUILD_DIR""$odd""$fileext" "$BUILD_DIR""$even"_"$odd"_"$model""$fileext"
     if [ "$?" == 0 ]; then mv "$BUILD_DIR""$even"_"$odd"_"$model""$fileext" "$DIST_DIR"; fi
-done < <(cat "$SRC_DIR"romlist.txt | sed '/^#/'d)
+done < <(cat "$BASE_DIR"romlist.txt | sed '/^#/'d)
