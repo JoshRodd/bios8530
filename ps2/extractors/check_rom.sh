fe1293ae4f59c0ab748c692fbfbac191e5c043a0
Josh Rodd <josh@rodd.us>
2020-09-27 16:42:00 -0400

    8x16 support

--- ps2/extractors/check_rom.sh
+++ ps2/extractors/check_rom.sh
@@ -3,8 +3,10 @@
 ./extract_rom_basic.sh || exit
 ./to_inc.py --asm > ROMBASIC.ASM || exit
 ./font_to_inc.py --asm > FONT8X8.ASM || exit
+./font_to_inc.py --asm --16 > FONT8X16.ASM || exit
 uasm -bin -Fo=ROMBASIC.BINTEST ROMBASIC.ASM || exit
 uasm -bin -Fo=FONT8X8.BINTEST FONT8X8.ASM || exit
+uasm -bin -Fo=FONT8X16.BINTEST FONT8X16.ASM || exit
 num=$(openssl dgst -sha256 ROMBASIC.BINTEST ROMBASIC.BIN | awk '{print $2}' | sort -u | wc -l)
 if [ $num != 1 ]; then
     printf "Warning: ROM BASIC images do not match. Check the following list.\n"
@@ -22,5 +24,14 @@ if [ $num != 1 ]; then
 else
     rm -f FONT8X8.BINTEST FONT8X8.BIN
     printf "FONT8X8.ASM checks valid - produces same image as FONT8X8.BIN\n"
+fi
+num=$(openssl dgst -sha256 FONT8X16.BINTEST FONT8X16.BIN | awk '{print $2}' | sort -u | wc -l)
+if [ $num != 1 ]; then
+    printf "Warning: 8x8 font images do not match. Check the following list.\n"
+    openssl dgst -sha256 FONT8X16.BINTEST FONT8X16.BIN
+    exit 1
+else
+    rm -f FONT8X16.BINTEST FONT8X16.BIN
+    printf "FONT8X16.ASM checks valid - produces same image as FONT8X16.BIN\n"
 fi
 exit 0
--- ps2/extractors/font_to_inc.py
+++ ps2/extractors/font_to_inc.py
@@ -4,21 +4,34 @@ import sys
 from functools import partial
 
 asm = 0
+height = 8
 if len(sys.argv) > 1 and sys.argv[1] == '--asm':
     asm = 1
 if len(sys.argv) > 1 and sys.argv[1] == '--inc':
     asm = 0
+if len(sys.argv) > 2 and sys.argv[2] == '--16':
+    height = 16
+
+bios_seg = 'F000'
+vector1 = 'FA6E'
+vector2 = 'FE6D'
+if height == 16:
+    vector1 = '4A30'
+    vector2 = '5A30'
 
 if asm:
     print("\
-                name    FONT8X8\n\
+                name    FONT8X{}\n\
             \n\
-; Typically placed at F000:FA6E - F000:FE6D\n\
+; Typically placed at {}:{} - {}:{}\n\
             \n\
-BIOS_FONT8X8    segment byte public 'BIOS_ROM'\n\
+BIOS_FONT8X{}{}   segment byte public 'BIOS_ROM'\n\
 \n\
-                org     0fa6eh\n\
-")
+                org     0{}h\n\
+".format(height,
+    bios_seg, vector1, bios_seg, vector2,
+    height, '' if height >= 10 else ' ',
+    vector1.lower()))
 
 chunk_size = 1
 padding_size = 8
@@ -32,7 +45,7 @@ while i < padding_size:
     ss += '\t'
     i += 1
 
-with open("FONT8X8.BIN", "rb") as f:
+with open("FONT8X{}.BIN".format(height), "rb") as f:
     for data in iter(partial(f.read, chunk_size), b''):
         x = int.from_bytes(data, byteorder='big')
         c = chr(x)
@@ -56,7 +69,7 @@ in_bare = 1
 i = padding_size
 while i < len(xx) - padding_size:
     out_buf = ''
-    cur_xx = (i - padding_size) // 8
+    cur_xx = (i - padding_size) // height
     cur_cc = chr(cur_xx)
     if cur_xx >= 0 and cur_xx < 32 or cur_xx >= 127:
         descr = 'CHR$({})'.format(cur_xx)
@@ -68,15 +81,15 @@ while i < len(xx) - padding_size:
         descr = '"{}"'.format(cur_cc)
     else:
         descr = 'Unknown character'
-    data = ','.join(str(x) for x in xx[i:i + 8])
-    spaces = ((len('255,') * 8 + len('db ') + len(' ')) - len(data)) * ' '
+    data = ','.join(str(x) for x in xx[i:i + height])
+    spaces = ((len('255,') * height + len('db ') + len(' ')) - len(data)) * ' '
     print("{}db {}{}; {}".format(tabs, data, spaces, descr))
-    i += 8
+    i += height
 
 if asm:
     print("\
 \n\
-BIOS_FONT8X8    ends\n\
+BIOS_FONT8X{}{}   ends\n\
 \n\
                 end\
-");
+".format(height, '' if height >= 10 else ''));
