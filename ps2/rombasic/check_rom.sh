7cacd0aefb09962e10da1dd53efc6d638f7378b0
Josh Rodd <josh@rodd.us>
2020-09-27 16:22:56 -0400

    Improvements

--- ps2/rombasic/check_rom.sh
+++ ps2/rombasic/check_rom.sh
@@ -3,7 +3,7 @@
 ./extract_rom_basic.sh || exit
 ./to_inc.py --asm > ROMBASIC.ASM || exit
 ./font_to_inc.py --asm > FONT8X8.ASM || exit
-uasm -bin -Fo=ROMBASIC.BINTEST TEST_ROM.ASM || exit
+uasm -bin -Fo=ROMBASIC.BINTEST ROMBASIC.ASM || exit
 uasm -bin -Fo=FONT8X8.BINTEST FONT8X8.ASM || exit
 num=$(openssl dgst -sha256 ROMBASIC.BINTEST ROMBASIC.BIN | awk '{print $2}' | sort -u | wc -l)
 if [ $num != 1 ]; then
@@ -13,15 +13,14 @@ if [ $num != 1 ]; then
 else
     rm -f ROMBASIC.BINTEST ROMBASIC.BIN
     printf "ROMBASIC.ASM checks valid - produces same image as ROMBASIC.BIN\n"
-    exit 0
 fi
 num=$(openssl dgst -sha256 FONT8X8.BINTEST FONT8X8.BIN | awk '{print $2}' | sort -u | wc -l)
 if [ $num != 1 ]; then
     printf "Warning: 8x8 font images do not match. Check the following list.\n"
-    openssl dgst -sha256 8X8FONT.BINTEST 8X8FONT.BIN
+    openssl dgst -sha256 FONT8X8.BINTEST FONT8X8.BIN
     exit 1
 else
-    rm -f 8X8FONT.BINTEST 8X8FONT.BIN
-    printf "8X8FONT.ASM checks valid - produces same image as 8X8FONT.BIN\n"
-    exit 0
+    rm -f FONT8X8.BINTEST FONT8X8.BIN
+    printf "FONT8X8.ASM checks valid - produces same image as FONT8X8.BIN\n"
 fi
+exit 0
--- ps2/rombasic/extract_rom_basic.sh
+++ ps2/rombasic/extract_rom_basic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 rm -f *_ROM_BASIC.BIN
-rm -f *_8X8FONT.BIN
+rm -f *_FONT8X8.BIN
 
 # F000:6000-F000:DFFF or
 # F600:0000-F000:7FFF
 for x in ../dist/*.BIN; do dd bs=1 skip=24576 count=32768 if=$x of="$(basename $x)"_ROM_BASIC.BIN; done 2>/dev/null
-for x in ../dist/*.BIN; do dd bs=1 skip=64110 count=1024 if=$x of="$(basename $x)"_8X8FONT.BIN; done 2>/dev/null
+for x in ../dist/*.BIN; do dd bs=1 skip=64110 count=1024 if=$x of="$(basename $x)"_FONT8X8.BIN; done 2>/dev/null
 
 num=$(openssl dgst -sha256 *_ROM_BASIC.BIN | awk '{print $2}' | sort -u | wc -l)
 if [ $num != 1 ]; then
@@ -20,16 +20,16 @@ else
     printf "BASIC in ROMBASIC.BIN\n"
 fi
 
-num=$(openssl dgst -sha256 *_8X8FONT.BIN | awk '{print $2}' | sort -u | wc -l)
+num=$(openssl dgst -sha256 *_FONT8X8.BIN | awk '{print $2}' | sort -u | wc -l)
 if [ $num != 1 ]; then
     printf "Warning: ROM BASIC images do not match. Check the following list.\n"
-    openssl dgst -sha256 *_8X8FONT.BIN
+    openssl dgst -sha256 *_FONT8X8.BIN
     exit 1
 else
-    fname="$(ls *_8X8FONT.BIN | head -1)"
-    mv "$fname" 8X8FONT.BIN || exit
-    rm -f *_8X8FONT.BIN || exit
-    printf "8x8 font in 8X8FONT.BIN\n"
+    fname="$(ls *_FONT8X8.BIN | head -1)"
+    mv "$fname" FONT8X8.BIN || exit
+    rm -f *_FONT8X8.BIN || exit
+    printf "8x8 font in FONT8X8.BIN\n"
 fi
 
 exit 0
